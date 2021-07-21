c0416ea1e4357dbaacbd3139fc7a56dc81c31d3c
mhumam <muhamadhumamm17@gmail.com>
2021-07-21 13:43:29 +0700

    initialize fetching data

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '361fdbc14afa017e7e7c15f6823460c9',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
